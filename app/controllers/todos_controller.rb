c70e67c82b957d5789d3d5eb5c4901d815d06ddb
rajkumarselva <rajkumar@qadex.com>
2015-04-20 06:33:01 +0000

    changes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,15 +1,12 @@
 class TodosController < ApplicationController
   def index
     @todo_items = Todo.all
-    #@todo_array = ["Buy Milk", "Buy Soap", "Pay EB Bill"]
   end
   
   def delete
     @todo_items =Todo.all 
     @t = Todo.last
     @t.delete
-    #put delete logic
-    #Todo.find(params[:id]).destroy
   end
   
   def add
